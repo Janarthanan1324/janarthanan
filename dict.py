41bf43520365fb6d125cdcdbdb93ca6f2c23fefd
Janarthanan.S <rthanan074@gmail.com>
2024-07-09 13:16:57 +0530

    Add files via upload
    my first task given by #mainflow create set,dict,list 
    and to modify,delete,add the values in the program

---
+++ dict.py
@@ -0,0 +1,5 @@
+my_dict= {'name':'jana','age':20,'city':'CBE'}
+my_dict['gender']='Male'
+my_dict['city']='theni'
+del my_dict['age']
+print("update dictionary:",my_dict)
---
+++ list.py
@@ -0,0 +1,9 @@
+#//creating my list
+my_list=[1,2,3,4,5]
+#//adding a new number
+my_list.append(6)
+#//remove a number in my list
+my_list.remove(3)
+#//modifying my list
+my_list[0]=10
+print("update list",my_list)
